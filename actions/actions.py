4179e256b6809c26875290143ee75751e443e39b
jfernandez <javiredracing@gmail.com>
2023-06-29 16:35:01 +0100

    First commit

--- actions/actions.py
+++ actions/actions.py
@@ -5,8 +5,6 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker#, FormValidationAction
@@ -29,6 +27,14 @@ class AuthenticatedAction(Action):
             return[ SlotSet("password",None), SlotSet("identifier",None), SlotSet("access_level", "0")]
         return []
 
+class DaysOff(Action):
+    def name(self) -> Text:
+        return "action_show_days_off"
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Mis dias libres")
+        return[]
+    
 '''        
 class ValidateAuthFormAction(FormValidationAction):
     def name(self) -> Text:
