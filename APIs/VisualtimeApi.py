4f90247a7c513b43fb93461e8d69b219e441b4d6
Javier Fernández Rodríguez <jfernandez@DESKTOP-V8A8VV3.ITER>
2023-07-03 18:56:42 +0100

    VIsualtime actions

---
+++ APIs/VisualtimeApi.py
@@ -0,0 +1,58 @@
+import urllib.request
+import urllib.parse
+import json
+from prettytable import PrettyTable
+import datetime
+
+class VisualtimeApi:
+    TOKEN = "YVhSbGNqUXhOemM9ODgwMDhhMDFlNjM3OTkxNTE1ODBmMmQzYWYxNzcxMDhmZjczNDljYzM4MzlmM2RmZDVlNTBkOWMyNzlhODQzNw%3D%3D"
+    URL = "https://vtliveapi.visualtime.net/api/v2/ScheduleService.svc/"
+    
+    @staticmethod
+    def getPublicHolidays():
+        contents = urllib.request.urlopen(VisualtimeApi.URL + "GetPublicHolidays?Token="+VisualtimeApi.TOKEN).read()
+        data_json = json.loads(contents) 
+        x = PrettyTable()
+        x.field_names = ["Festivo", "Fecha", "Tipo"]
+        for days in data_json["Value"]:
+            title = days["ID"]
+            for day in days["PublicHolidaysDetails"]:
+                datetime1 = datetime.datetime.strptime(day["PublicHolidayDate"]["Data"].split(" ")[0], '%Y-%m-%d').strftime('%A, %d %B %Y')
+                x.add_row([day["Description"], datetime1, title])                          
+        return x.get_string()
+        
+    @staticmethod
+    def getHolidays(identifier, date1 = None, date2 = None):
+            
+        start_date = "2023-01-05%2007%3A30%3A00%2B02"
+        end_date = "2023-09-07%2012%3A30%3A00%2B02"
+        if date1 == None:
+            today = datetime.date.today()
+            year = today.year
+            start_date = '{}-01-01 00:00:00 +01'.format(year)
+            print (start_date)
+           
+        if date2 == None :
+            today = datetime.date.today()
+            if date1 == None:                
+                year = today.year
+                end_date = '{}-12-31 23:59:50 +01'.format(year)
+                print (end_date)
+            else:
+                t = datetime.datetime(today.year + 1, today.month, 1, 0, 0)
+                endDate = t.strftime('%Y-%m-%d H%:M%:S%') + " +01"
+                
+        #if date1 < date2:        
+        start_date = urllib.parse.quote(start_date)
+        end_date = urllib.parse.quote(end_date)              
+        employeeID = "457"
+        contents = urllib.request.urlopen(VisualtimeApi.URL + "GetHolidays?Token=" + VisualtimeApi.TOKEN + "&StartDate=" + start_date + "&EndDate=" + end_date + "&EmployeeID=" + employeeID).read()
+        data_json = json.loads(contents) 
+        x = PrettyTable()
+        x.field_names = ["Fecha", "Motivo"]
+        for day in data_json["Value"]:
+            datetime1 = datetime.datetime.strptime(day["PlannedDate"]["Data"].split(" ")[0], '%Y-%m-%d').strftime('%A, %d %B %Y')
+            x.add_row([datetime1, day["ReasonName"]])
+        return x.get_string()    
+        #else:
+            #return "Init date must be higher than ending date"
--- actions/actions.py
+++ actions/actions.py
@@ -11,17 +11,19 @@ from rasa_sdk import Action, Tracker#, FormValidationAction
 from rasa_sdk.events import SlotSet, AllSlotsReset
 from rasa_sdk.executor import CollectingDispatcher
 
+from APIs.VisualtimeApi import *
+
 class AuthenticatedAction(Action):
     def name(self) -> Text:
         return "action_authenticated"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        email = tracker.get_slot("identifier")
+        identifier = tracker.get_slot("identifier")
         password = tracker.get_slot("password")
         access_level = "1"
-        if email is not None and password == "1111":
+        if identifier is not None and password == "1111":
             dispatcher.utter_message(response="utter_authenticated_successfully")
-            return [SlotSet("access_level", access_level), SlotSet("password","")]
+            return [SlotSet("access_level", access_level), SlotSet("password","1111")]
         else:
             dispatcher.utter_message(response="utter_authentication_failure")
             return[ SlotSet("password",None), SlotSet("identifier",None), SlotSet("access_level", "0")]
@@ -32,15 +34,38 @@ class DaysOff(Action):
         return "action_show_days_off"
     
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Mis dias libres")
+        result = VisualtimeApi.getPublicHolidays()
+        dispatcher.utter_message(text = result)
         return[]
+        
+class Holidays(Action):
+    def name(self) -> Text:
+        return "action_show_holidays"
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        identifier = tracker.get_slot("identifier")
+        if identifier != None:
+            code = 457
+            result = VisualtimeApi.getHolidays(code)
+            dispatcher.utter_message(text = result)
+        else:
+            dispatcher.utter_message(response = "utter_authentication_failure")
+        return[]
+
+class ClearLogin(Action): 
+    def name(self) -> Text:
+        return "action_clear_login"
+        
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return[ SlotSet("password",None), SlotSet("identifier",None), SlotSet("access_level", "0")]
+        
     
 '''        
 class ValidateAuthFormAction(FormValidationAction):
     def name(self) -> Text:
         return "validate_auth_form"
 
-    #def validate_email(self, value: Text, dispatcher: "CollectingDispatcher", tracker: "Tracker", domain: "DomainDict",) -> List[EventType]:
+    #def validate_identifier(self, value: Text, dispatcher: "CollectingDispatcher", tracker: "Tracker", domain: "DomainDict",) -> List[EventType]:
     def validate_identifier(self, value: Text, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict,) -> Dict[Text, Any]:
         returned_slots = {}    
         if value is not None: #check if valid
@@ -48,9 +73,9 @@ class ValidateAuthFormAction(FormValidationAction):
         else:
             returned_slots = {"identifier": None}
             if value is None:
-                dispatcher.utter_message(template="utter_email_not_valid")
+                dispatcher.utter_message(template="utter_identifier_not_valid")
             elif not is_valid_user(value):
-                dispatcher.utter_message(template="utter_email_not_registered")
+                dispatcher.utter_message(template="utter_identifier_not_registered")
         return returned_slots
     
     def validate_password(self, value: TEXT, dispatcher: "CollectingDispatcher", tracker: "Tracker", domain: "DomainDict",) -> Dict[Text, Any]:
